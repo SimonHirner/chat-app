449bced8a9381d7ee5bf030a53269b329f56d193
SimonHirner <simon.hirner@hm.edu>
2019-10-31 11:59:15 +0100

    Erste funktionsfähige Implementierung des AuditLogServers auf TCP-Basis.

--- chatApplication Vorgabe WS 19 20/src/main/java/edu/hm/dako/chat/AuditLogServer/AuditLogTcpServer.java
+++ chatApplication Vorgabe WS 19 20/src/main/java/edu/hm/dako/chat/AuditLogServer/AuditLogTcpServer.java
@@ -1,6 +1,16 @@
 package edu.hm.dako.chat.AuditLogServer;
 
 import edu.hm.dako.chat.common.AuditLogPDU;
+
+//Zusätzliche Imports
+import edu.hm.dako.chat.connection.EndOfFileException;
+import edu.hm.dako.chat.tcp.TcpConnection;
+import edu.hm.dako.chat.tcp.TcpServerSocket;
+
+//Zusätzliche Imports für FileWriter
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
@@ -35,5 +45,44 @@ public class AuditLogTcpServer {
 		log.info("AuditLog-TcpServer gestartet, Port: " + AUDIT_LOG_SERVER_PORT);
 
 		//TODO: Implementierung des AuditLogServers auf TCP-Basis hier ergaenzen
+		
+		try {
+			//Server Socket für AuditLogServer erzeugen
+			TcpServerSocket auditLogServerSocket = new TcpServerSocket(AUDIT_LOG_SERVER_PORT, DEFAULT_SENDBUFFER_SIZE, DEFAULT_RECEIVEBUFFER_SIZE);
+			//Verbindung mit ChatServer erzeugen und aufbauen
+			TcpConnection auditLogServerConnection = (TcpConnection) auditLogServerSocket.accept();
+			
+			//FileWriter erzeugen	
+            FileWriter fileWriter = new FileWriter("ChatAuditLog.dat");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            //Kopf von AuditLogFile erstellen
+            bufferedWriter.write("-------- AuditLog: --------");
+            bufferedWriter.newLine();
+             
+            //Empfangene AuditLogPDUs in AuditLogFile schreiben
+            try {
+            	AuditLogPDU receivedAuditLogPDU = null;
+            	do {
+            		receivedAuditLogPDU = (AuditLogPDU) auditLogServerConnection.receive(100000);
+            		bufferedWriter.write(receivedAuditLogPDU.toString());
+            	} while (receivedAuditLogPDU != null);
+            } catch (EndOfFileException e) {
+            	//Ordnungsgemaesses Beenden wegen Verbindungsabruch
+    			bufferedWriter.close();
+    			auditLogServerConnection.close();
+    			auditLogServerSocket.close();
+            }
+            
+            //Ordnungsgemäßes Beenden wegen Timeout
+			bufferedWriter.close();
+			auditLogServerConnection.close();
+			auditLogServerSocket.close();
+			
+		} catch (Exception exception) {
+			//Andere Exceptions behandeln
+			System.out.println("Schwerwiegender Fehler!");
+		}
+		
 	}
 }
