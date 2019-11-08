ffaab129e2dbc699a4272b4e36adb299dd15ed1c
armin <armin@c077.hm.vpn.lrz.de>
2019-11-08 10:31:01 +0100

    Kommentare wurden hinzugefügt

--- chatApplication Vorgabe WS 19 20/src/main/java/edu/hm/dako/chat/AuditLogServer/Administration.java
+++ chatApplication Vorgabe WS 19 20/src/main/java/edu/hm/dako/chat/AuditLogServer/Administration.java
@@ -4,19 +4,21 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+
+/**
+ * 
+ * Die Klasse wertet die ChatAuditLog.txt aus
+ * 
+ * @author Armin Lewig
+ *
+ */
 
 public class Administration {
 	
-	private static void ladeDatei(String datName) {
-		
-		
+	private static void ladeDatei(String fileName) {	
 		// Zaehler fuer ankommende AuditLog-PDUs
 		long counterAuditlog = 0;	
-		
-		// alle Benutzernamen
-		ArrayList<String> userNames = new ArrayList<>();
-		
+	
 		//Zaehler AuditLogType Login
 		int counterLogin = 0;
 		
@@ -27,37 +29,43 @@ public class Administration {
 		int counterChat = 0;
 		
 
-        File file = new File(datName);
+        File file = new File(fileName);
 
+        //wenn die Datei nicht gelesen oder es kein File ist, wird hier geendet
         if (!file.canRead() || !file.isFile())
             System.exit(0);
 
             BufferedReader in = null;
         try {
-            in = new BufferedReader(new FileReader(datName));
-            String zeile = null;
-            while ((zeile = in.readLine()) != null) {
-            	if(zeile.contains("AuditLogPdu")) {
+            in = new BufferedReader(new FileReader(fileName));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+            	//es wird ueberprueft, ob "AuditLogPdu" in der Zeile steht und der entsprechende Zaehler wird hochgezaehlt
+            	if(line.contains("AuditLogPdu")) {
             		counterAuditlog++;
             	}
-            	if(zeile.contains("Login")) {
+            	//es wird ueberprueft, ob "Login" in der Zeile steht und der entsprechende Zaehler wird hochgezaehlt
+            	if(line.contains("Login")) {
             		counterLogin++;
             	}
-            	if(zeile.contains("Chat")) {
+            	//es wird ueberprueft, ob "Chat" in der Zeile steht und der entsprechende Zaehler wird hochgezaehlt
+            	if(line.contains("Chat")) {
             		counterChat++;
             	}
-            	if(zeile.contains("Logout")) {
+            	//es wird ueberprueft, ob "Logout" in der Zeile steht und der entsprechende Zaehler wird hochgezaehlt
+            	if(line.contains("Logout")) {
             		counterLogout++;
             	}
-//                System.out.println(zeile);
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
+        	//BufferedReader wird geschlossen
             if (in != null)
                 try {
                     in.close();
                 } catch (IOException e) {
+                	System.out.println("BufferedReader konnte nicht geschlossen werden");
                 }
         }
         System.out.println("Anzahl der AuditLog-PDUs: " + counterAuditlog);
@@ -68,7 +76,7 @@ public class Administration {
 	
 	
 	public static void main(String[] args) {
-        String dateiName = "ChatAuditLog.txt";
-        ladeDatei(dateiName);
+        String fileName = "ChatAuditLog.txt";
+        ladeDatei(fileName);
     }
 }
