2c91a14e2ee69492a60852a0fb0f3ec3a0c8c4a8
Armin Lewig <a.lewig@gmx.de>
2019-11-17 15:14:08 +0100

    AuditLog Auswertung wird laufend aktualisiert und kleine Anpassungen

--- chatApplication Vorgabe WS 19 20/src/main/java/edu/hm/dako/chat/AuditLogServer/Administration.java
+++ chatApplication Vorgabe WS 19 20/src/main/java/edu/hm/dako/chat/AuditLogServer/Administration.java
@@ -1,5 +1,6 @@
 package edu.hm.dako.chat.AuditLogServer;
 
+import java.awt.Container;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -12,36 +13,42 @@ import javax.swing.JLabel;
  * 
  * Die Klasse wertet die ChatAuditLog.txt aus
  * 
- * @author Armin Lewig
  *
  */
 
 public class Administration extends JFrame {
 
 	// Zaehler fuer ankommende AuditLog-PDUs
-	static long counterAuditlog = 0;
+	static long counterAuditlog;
 	// Zaehler AuditLogType Login
-	static int counterLogin = 0;
+	static int counterLogin;
 	// Zaehler AuditLogType Logout
-	static int counterLogout = 0;
+	static int counterLogout;
 	// Zaehler AuditLogType Chat
-	static int counterChat = 0;
+	static int counterChat;
 
-	//GUI
+	// GUI
 	static JFrame information;
-	//Text für die GUI
+	// Text für die GUI
 	static JLabel auditLogPdus;
 	static JLabel logins;
 	static JLabel messages;
 	static JLabel logouts;
-	
+	private static boolean isGuiOpen = false;
+
 	public static void main(String[] args) {
 		String fileName = "ChatAuditLog.dat";
-		read(fileName);
+		readAuditLog(fileName);
 	}
 
-	//die Methode liest Zeile für Zeile die mitgegebene Datei
-	static void read(String fileName) {
+	// die Methode liest Zeile für Zeile die mitgegebene Datei
+	static void readAuditLog(String fileName) {
+		
+		//setzt Zaehler auf null
+		counterAuditlog = 0;
+		counterLogin = 0;
+		counterLogout = 0;
+		counterChat = 0;
 
 		File file = new File(fileName);
 
@@ -92,31 +99,40 @@ public class Administration extends JFrame {
 	// zeigt in einem Fenster die Auswertung an
 	private static void gui() {
 
-		information = new JFrame("AuditLog Auswertung");
+		if (!isGuiOpen) {
+			information = new JFrame("AuditLog Auswertung");
 
-		// setzt die Einstellungen für das Fenster
-		information.setLayout(null);
-		information.setVisible(true);
-		information.setSize(400, 400);
-		information.setResizable(false);
-		information.setLocationRelativeTo(null);
-		information.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			// setzt die Einstellungen für das Fenster
+			information.setLayout(null);
+			information.setVisible(true);
+			information.setSize(400, 400);
+			information.setResizable(false);
+			information.setLocationRelativeTo(null);
+			information.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		auditLogPdus = new JLabel("Anzahl der AuditLog-PDUs: " + counterAuditlog);
-		auditLogPdus.setBounds(5, -30, 400, 100);
-		information.add(auditLogPdus);
+			auditLogPdus = new JLabel("Anzahl der AuditLog-PDUs: " + counterAuditlog);
+			auditLogPdus.setBounds(5, -30, 400, 100);
+			information.add(auditLogPdus);
 
-		logins = new JLabel("Anzahl der Logins: " + counterLogin);
-		logins.setBounds(5, -10, 400, 100);
-		information.add(logins);
+			logins = new JLabel("Anzahl der Logins: " + counterLogin);
+			logins.setBounds(5, -10, 400, 100);
+			information.add(logins);
 
-		messages = new JLabel("Anzahl der Chat Nachrichten: " + counterChat);
-		messages.setBounds(5, 10, 400, 100);
-		information.add(messages);
+			messages = new JLabel("Anzahl der Chat Nachrichten: " + counterChat);
+			messages.setBounds(5, 10, 400, 100);
+			information.add(messages);
 
-		logouts = new JLabel("Anzahl der Logouts: " + counterLogout);
-		logouts.setBounds(5, 30, 400, 100);
-		information.add(logouts);
+			logouts = new JLabel("Anzahl der Logouts: " + counterLogout);
+			logouts.setBounds(5, 30, 400, 100);
+			information.add(logouts);
 
+			isGuiOpen = true;
+		} else {
+			//aktualisiert den Text im Fenster
+			auditLogPdus.setText("Anzahl der AuditLog-PDUs: " + counterAuditlog);
+			logins.setText("Anzahl der Logins: " + counterLogin);
+			messages.setText("Anzahl der Chat Nachrichten: " + counterChat);
+			logouts.setText("Anzahl der Logouts: " + counterLogout);
+		}
 	}
 }
--- chatApplication Vorgabe WS 19 20/src/main/java/edu/hm/dako/chat/AuditLogServer/AuditLogTcpServer.java
+++ chatApplication Vorgabe WS 19 20/src/main/java/edu/hm/dako/chat/AuditLogServer/AuditLogTcpServer.java
@@ -1,14 +1,15 @@
 package edu.hm.dako.chat.AuditLogServer;
 
-// Zusätzliche Imports
+// Zusï¿½tzliche Imports
 import edu.hm.dako.chat.common.AuditLogPDU;
 import edu.hm.dako.chat.connection.ConnectionTimeoutException;
 import edu.hm.dako.chat.connection.EndOfFileException;
 import edu.hm.dako.chat.tcp.TcpConnection;
 import edu.hm.dako.chat.tcp.TcpServerSocket;
 
-// Zusätzliche Imports für FileWriter
+// Zusï¿½tzliche Imports fï¿½r FileWriter
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 
 import org.apache.log4j.Logger;
@@ -42,6 +43,12 @@ public class AuditLogTcpServer {
 	static final int CONNECTION_WAITING_TIME = 150000;
 
 	public static void main(String[] args) {
+				
+		// Falls eine alte AuditLog Datei vorhanden ist, wird diese gelÃ¶scht
+		File auditLogFile = new File("ChatAuditLog.dat");
+		if(auditLogFile.exists()) {
+			auditLogFile.delete();
+		}
 
 		PropertyConfigurator.configureAndWatch("log4j.auditLogServer_tcp.properties", 60 * 1000);
 		System.out.println("AuditLog-TcpServer gestartet, Port: " + AUDIT_LOG_SERVER_PORT);
@@ -51,7 +58,7 @@ public class AuditLogTcpServer {
 		
 		try {
 		
-			// Server Socket für AuditLogServer erzeugen
+			// Server Socket fï¿½r AuditLogServer erzeugen
 			TcpServerSocket auditLogServerSocket = new TcpServerSocket(AUDIT_LOG_SERVER_PORT, DEFAULT_SENDBUFFER_SIZE, DEFAULT_RECEIVEBUFFER_SIZE);
 			
 			// Verbindung mit ChatServer erzeugen und aufbauen
@@ -68,6 +75,8 @@ public class AuditLogTcpServer {
             		receivedAuditLogPDU = (AuditLogPDU) auditLogServerConnection.receive(CONNECTION_WAITING_TIME);
             		bufferedWriter.write(receivedAuditLogPDU.toString());
             		bufferedWriter.flush();
+            		//liest AuditLog Datei
+            		Administration.readAuditLog("ChatAuditLog.dat");
             	} catch (EndOfFileException endOfFileException) {
             		//Verbindungsabbruch
             		System.out.println("Verbindungsabbruch");
